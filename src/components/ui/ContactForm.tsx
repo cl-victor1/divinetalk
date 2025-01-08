753a33bcd8104b8143af28d2d5ca6fcc742773c6
Victor <cheng_long_@126.com>
2025-01-08 23:22:23 +0800

    fixed the linter error by replacing the unescaped apostrophe (') with the HTML entity &apos

--- src/components/ui/ContactForm.tsx
+++ src/components/ui/ContactForm.tsx
@@ -78,7 +78,7 @@ export default function ContactForm() {
   if (submitSuccess) {
     return (
       <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded">
-        Thank you for your message. We'll get back to you soon!
+        Thank you for your message. We&apos;ll get back to you soon!
       </div>
     );
   }
